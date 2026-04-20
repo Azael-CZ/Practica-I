47361f67b523922a9c9088620621b6e438fc4e4e
Azael Candelaria <jair4azael@gmail.com>
2026-04-19 19:26:29 -0600

    Cree la clase y la funcion init

---
+++ AzaelCandelaria/PracticaUno.py
@@ -0,0 +1,7 @@
+class Hambuguesa:
+    def __init__ (self, id, nombre, precio, stock, descuento):
+        self.__id = id
+        self.__nombre = nombre
+        self.__precio = precio
+        self.__stock = stock
+        self.__descuento = descuento
