62418dbdb01be7fa53ca12f24d72aa7489be7b26
Azael Candelaria <jair4azael@gmail.com>
2026-04-20 18:01:20 -0600

    Agregue la funcion 'Info()'

--- AzaelCandelaria/PracticaUno.py
+++ AzaelCandelaria/PracticaUno.py
@@ -1,7 +1,28 @@
 class Hambuguesa:
-    def __init__ (self, id, nombre, precio, stock, descuento):
+    # Crear el init y la clase
+    def __init__ (self, id, nombre, precio, stock, descuento, ingredientes=[]):
         self.__id = id
         self.__nombre = nombre
         self.__precio = precio
         self.__stock = stock
         self.__descuento = descuento
+        self.__ingredientes = ingredientes
+
+    # Funcion Info(Imprimir la informacion de la hamburguesa)
+    def info (self):
+        print(f"--Detalles de la hamburguesa-- \nLa hamgurguesa {self.__nombre} tiene un precio de {self.__precio} \nSu ID es: {self.__id} y hay en el stock {self.__stock} piezas")
+        if self.__descuento >= 1 and self.__descuento <=100:
+            print(f"La hamburguesa tiene un descuento de {self.__descuento}%")
+        else:
+            print("No tiene un descuento actualmente")
+
+    # Funcion Valor_inventario(Calcular el precio * el stock)
+    
+    # Funcion Precio_descuento(Calcular el precio si tiene descuento)
+
+    # Funcion Ingredientes(Imprimir los ingredientes de la hamburguesa)
+
+
+    # Gets y Sets
+
+    # Menu breve
